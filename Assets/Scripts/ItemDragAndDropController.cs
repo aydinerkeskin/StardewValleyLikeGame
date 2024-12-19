f7ed4e2e6f85992d1cd19d4f60a72b0fdaabef1e
Aydın Erkeskin <aydinerkeskin@gmail.com>
2024-12-19 23:31:49 +0300

    Sürükleme efekti için geliştirme tamamlandı.

--- Assets/Scripts/ItemDragAndDropController.cs
+++ Assets/Scripts/ItemDragAndDropController.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemDragAndDropController : MonoBehaviour
 {
     [SerializeField] ItemSlot itemSlot;
+    [SerializeField] GameObject itemIcon;
+
+    RectTransform iconTransform;
+    Image itemIconImage;
 
     private void Start()
     {
         itemSlot = new ItemSlot();
+        iconTransform = itemIcon.GetComponent<RectTransform>();
+        itemIconImage = itemIcon.GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (itemIcon.activeInHierarchy == true)
+        {
+            iconTransform.position = Input.mousePosition;
+        }
     }
 
     public void OnClick(ItemSlot newItemSlot)
@@ -26,6 +41,22 @@ public class ItemDragAndDropController : MonoBehaviour
             newItemSlot.Copy(itemSlot);
 
             itemSlot.Set(item, count);
+        }
+
+        UpdateIcon();
+    }
+
+    private void UpdateIcon()
+    {
+        if (itemSlot.item == null)
+        {
+            itemIcon.SetActive(false);
+            itemIconImage.sprite = null;
+        }
+        else
+        {
+            itemIcon.SetActive(true);
+            itemIconImage.sprite = itemSlot.item.Icon;
         }
     }
 }
